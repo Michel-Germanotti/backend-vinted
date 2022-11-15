7bf796fc92f77d4fbcb1c86ddacb19f623652bae
Michel-Germanotti <michel.gmv@outlook.fr>
2022-11-15 18:02:55 +0100

    repair serve

--- routes/offer.js
+++ routes/offer.js
@@ -8,109 +8,257 @@ const cloudinary = require('cloudinary').v2;
 
 const router = express.Router();
 
-// Return "https" URLs by setting secure: true
-cloudinary.config({ 
-    cloud_name: 'dnnjwgolt', 
-    api_key: '969599212569319', 
-    api_secret: 'FRIFwUy3nQsfYtzLDXUGcaf4nFE',
-    secure: true
-  });
-
 // Create
-    router.post('/offer/publish', isAuthenticated, fileUpload(), async (req,res) => {
-        try {
-            const {title, description, price, condition, city, brand, size, color} = req.body;
-
-            const newOffer = new Offer({
-                product_name: title,
-                product_description: description,
-                product_price: price,
-                product_details: [
-                { MARQUE: brand },
-                { TAILLE: size },
-                { ÉTAT: condition },
-                { COULEUR: color },
-                { EMPLACEMENT: city },
-                ],
-                owner: req.user,
-            });
-
-            if (req.files?.picture) {
-                const result = await cloudinary.uploader.upload(
-                    // convert picture
-                    convertToBase64(req.files.picture), {
-                        folder: "/vinted/offers"
-                    }
-                );
-                newOffer.product_image = result;
-              }
-
-            await newOffer.save();
-            res.status(200).json({newOffer});
-        } catch (error) {
-            res.status(400).json({message : error.message}); 
+router.post(
+    "/offer/publish",
+    isAuthenticated,
+    //upload.array("photos", 3),
+    fileUpload(),
+    async (req, res) => {
+      //console.log(req.body);
+      try {
+        const { title, description, price, brand, size, condition, color, city } =
+          req.body;
+        if (
+          !title ||
+          !description ||
+          !price ||
+          !brand ||
+          !size ||
+          !city ||
+          !color ||
+          !condition
+        ) {
+          return res
+            .status(400)
+            .json({ message: "One of the element is missing" });
         }
-    })
-
-// Read
-    router.get('/offers', async (req, res) => {
-        try {
-            const {title, priceMin, priceMax, sort, page} = req.query;
-                
-                /* ----------- FILTERS ----------- */ 
-                const filters = {};
-
-                if(title){
-                    filters.product_name = new RegExp(title, 'i');
-                }
-
-                if(priceMin){
-                    filters.product_price = {$gte: Number(priceMin) };
-                }
-                
-                if(priceMax){
-                    if(!filters.product_price){
-                        filters.product_price = {$lte: Number(priceMax) };
-                    } else {
-                        filters.product_price.$lte = Number(priceMax);
-                    }
-                }
-
-                // asc || desc
-                const sortFilter = {}
-                if(sort === "price-desc"){
-                    sortFilter.product_price = "desc";
-                } else if(sort === "price-asc"){
-                    sortFilter.product_price = "asc";
-                }
-
-                // 5 résults per page : 1 skip 0, 2 skip 5, 3 skip 10, 4 skip 15
-                // 3 résults per page : 1 skip 0, 2 skip 3, 3 skip 6, 4 skip 9
-                const limit = 5;
-                let pageRequired = 1;
-
-                if(page){
-                    pageRequired = Number(page);
-                }
-                
-                const skip = (pageRequired - 1) * limit;
-
-                /* ----------- END FILTERS ----------- */ 
-            
-            const offers = await Offer.find(filters)
-                .sort(sortFilter)
-                .skip(skip)
-                .limit(limit)
-                .select("product_name product_price owner")
-                .populate("owner", "account _id");
-
-            // number of results
-            const offerCount = await Offer.countDocuments(filters)
-
-            res.json({count: offerCount, offers: offers});
-        } catch (error) {
-            res.status(400).json({message : error.message});  
+  
+        const newOffer = new Offer({
+          product_name: req.body.title,
+          product_description: req.body.description,
+          product_price: req.body.price,
+          product_details: [
+            {
+              MARQUE: req.body.brand,
+            },
+            {
+              TAILLE: req.body.size,
+            },
+            {
+              ETAT: req.body.condition,
+            },
+            {
+              COULEUR: req.body.color,
+            },
+            {
+              EMPLACEMENT: req.body.city,
+            },
+          ],
+  
+          owner: req.user,
+        });
+        //console.log(req.files.avatar);
+  
+        if (!req.files?.avatar) {
+          return res.status(400).json({ message: "No image contained" });
         }
-    });
-
-module.exports = router;
+        const avatarConverted = convertToBase64(req.files.avatar);
+        const resultAvatar = await cloudinary.uploader.upload(avatarConverted, {
+          folder: `/vinted/offers${newOffer._id}`,
+        });
+        newOffer.product_image = resultAvatar;
+  
+        //console.log(avatarConverted);
+        //console.log(resultAvatar);
+        //console.log(req.files.picture.length);
+  
+        if (!req.files?.picture) {
+          return res.status(400).json("No images uploaded!");
+        }
+  
+        const arrayOfPictureUrl = [];
+        const picture = req.files.picture;
+        let pictureConverted = "";
+        let resultPicture = {};
+        const length = req.files.picture.length;
+        for (let i = 0; i < length; i++) {
+          pictureConverted = convertToBase64(picture[i]);
+          //console.log(pictureConverted);
+          resultPicture = await cloudinary.uploader.upload(pictureConverted, {
+            folder: `/vinted/offers_${newOffer._id}`,
+          });
+          console.log(resultPicture);
+          arrayOfPictureUrl.push(resultPicture.secure_url);
+        }
+        //console.log(arrayOfPictureUrl);
+  
+        //if (arrayOfPictureUrl.length === req.files.picture.length)
+  
+        //console.log(resultImage);
+        newOffer.product_pictures = arrayOfPictureUrl;
+        await newOffer.save();
+  
+        res.status(200).json(newOffer);
+      } catch (error) {
+        return res.status(400).json({ error: error.message });
+      }
+    }
+  );
+  
+  router.put(
+    "/offer/modify",
+    isAuthenticated,
+    fileUpload(),
+    async (req, res) => {
+      if (!req.body.id) {
+        return res.status(400).json({ message: "The offre isn't chosed" });
+      }
+  
+      //const { title, description, price, brand, size, condition, color, city } =
+      const offerUpdate = await Offer.findById(req.body.id).populate({
+        path: "owner",
+        select: "account",
+      });
+  
+      if (!offerUpdate) {
+        return res.status(400).json({ message: "No offer with this ID" });
+      }
+      //req.body;
+  
+      if (req.body.description) {
+        offerUpdate.product_description = req.body.description;
+      }
+      if (req.body.price) {
+        offerUpdate.product_price = req.body.price;
+      }
+      if (req.body.title) {
+        offerUpdate.product_name = req.body.title;
+      }
+      if (req.body.brand) {
+        offerUpdate.product_details[0].MARQUE = req.body.brand;
+      }
+      if (req.body.size) {
+        offerUpdate.product_details[1].TAILLE = req.body.size;
+      }
+      if (req.body.condition) {
+        offerUpdate.product_details[2].ETAT = req.body.condition;
+      }
+      if (req.body.color) {
+        offerUpdate.product_details[3].COULEUR = req.body.color;
+      }
+      if (req.body.city) {
+        offerUpdate.product_details[4].EMPLACEMENT = req.body.city;
+      }
+      if (req.files.picture) {
+        await cloudinary.uploader.destroy(offerUpdate.product_image.public_id);
+        const pictureUpdateConverted = convertToBase64(req.files.picture);
+        const resultImageUpdate = await cloudinary.uploader.upload(
+          pictureUpdateConverted,
+          {
+            folder: `/vinted/offers${offerUpdate._id}`,
+          }
+        );
+        offerUpdate.product_image = resultImageUpdate;
+      }
+  
+      offerUpdate.markModified("product_details");
+      await offerUpdate.save();
+      res.status(200).json(offerUpdate);
+    }
+  );
+  
+  router.delete("/offer/delete", isAuthenticated, async (req, res) => {
+    //console.log(req.body.id);
+    if (!req.body.id) {
+      return res.status(400).json({ error: error.message });
+    }
+  
+    const offerToDelete = await Offer.findById(req.body.id);
+  
+    if (req.files.picture) {
+      await cloudinary.uploader.destroy(offerToDelete.product_image.public_id);
+  
+      await offerToDelete.delete();
+      res
+        .status(200)
+        .json({ message: "The offre have been succesefully deleted" });
+      //console.log(offerToDelete);
+    }
+  });
+  
+  router.get("/offers", async (req, res) => {
+    try {
+      const filters = {};
+      let sortObj = {};
+      let filtre_price;
+      //console.log(req.query.sort);
+      let filterOffers;
+      if (req.query.priceMin && req.query.priceMax) {
+        filters.product_price = {
+          $gte: Number(req.query.priceMin),
+          $lte: Number(req.query.priceMax),
+        };
+      } else if (req.query.priceMin) {
+        filters.product_price = { $gte: Number(req.query.priceMin) };
+      } else if (req.query.priceMax) {
+        filters.product_price = { $lte: Number(req.query.priceMax) };
+      }
+  
+      if (req.query.title) {
+        const middleFilter = req.query.title;
+        filters.product_name = RegExp(middleFilter, "i");
+      }
+      if (req.query.sort === "price - asc" || req.query.sort === "asc") {
+        filtre_price = 1;
+      } else if (req.query.sort === "price - dsc" || req.query.sort === "dsc") {
+        filtre_price = -1;
+      }
+  
+      sortObj = { product_price: filtre_price };
+  
+      const limit = 4;
+      let pageRequired = 1;
+  
+      if (req.query.page) {
+        pageRequired = Number(req.query.page);
+      }
+  
+      const skip = (Number(req.query.page) - 1) * limit;
+      //console.log("sortObj", sortObj);
+      //console.log(filters);
+      filterOffers = await Offer.find(filters)
+        .populate({ path: "owner", select: "account" })
+        .sort(sortObj)
+        //.select("_id product_name product_price owner")
+        .skip(skip)
+        .limit(limit);
+  
+      //console.log("filterOffers", filterOffers);
+  
+      const count = await Offer.countDocuments(filters);
+  
+      res.status(200).json({ count, filterOffers });
+    } catch (error) {
+      res.status(404).json({ message: error.message });
+    }
+  });
+  
+  router.get("/offer/:id", async (req, res) => {
+    try {
+      const detailOffer = await Offer.findById(req.params.id).populate({
+        path: "owner",
+        select: "account",
+      });
+  
+      if (!detailOffer) {
+        return res.status(400).json({ message: "No article with this ID" });
+      }
+  
+      res.status(200).json(detailOffer);
+      //console.log(req.params.id);
+    } catch (error) {
+      res.status(404).json({ message: error.message });
+    }
+  });
+  module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,112 +1,104 @@
 const express = require("express");
-const fileUpload = require("express-fileupload");
-const convertToBase64 = require("../utils/convertToBase64"); // Fonction qui permet de transformer nos fichier qu'on reçois sous forme de Buffer en base64 afin de pouvoir les upload sur cloudinary
-
-const uid2 = require("uid2"); // Package qui sert à créer des string aléatoires
-const SHA256 = require("crypto-js/sha256"); // Sert à encripter une string
-const encBase64 = require("crypto-js/enc-base64"); // Sert à transformer l'encryptage en string
-
 const router = express.Router();
+const SHA256 = require("crypto-js/sha256");
+const encBase64 = require("crypto-js/enc-base64");
+const uid2 = require("uid2");
+const cloudinary = require("cloudinary").v2;
+const fileUpload = require("express-fileupload");
 
 const User = require("../models/User");
 
-// upload file
+const convertToBase64 = (file) => {
+  return `data:${file.mimetype};base64,${file.data.toString("base64")}`;
+};
+router.post("/user/signup", fileUpload(), async (req, res) => {
+  //console.log(req.body);
+  //console.log(req.files);
+  try {
+    if (
+      !req.body.email ||
+      !req.body.username ||
+      !req.body.password ||
+      req.body.newsletter
+    ) {
+      return res.status(400).json({ message: "The element missing" });
+    }
 
-const cloudinary = require('cloudinary').v2;
+    if (await User.findOne({ email: req.body.email })) {
+      return res.status(400).json({ message: "This mail has already used" });
+    }
+    const password = req.body.password;
+    const salt = uid2(16);
+    const hash = SHA256(salt + password).toString(encBase64);
+    const token = uid2(64);
 
-// Return "https" URLs by setting secure: true
-cloudinary.config({ 
-    cloud_name: 'dnnjwgolt', 
-    api_key: '969599212569319', 
-    api_secret: 'FRIFwUy3nQsfYtzLDXUGcaf4nFE',
-    secure: true
-  });
-
-
-// Create 
-    // signup
-    router.post('/user/signup', fileUpload(), async (req,res) => {
-        try {
-            const { username, email, password, newsletter } = req.body;
-            // allready information has been mentionned ? 
-            if(!username ||
-                !email ||
-                !password ||
-                (newsletter !== false && newsletter !== true)
-                ) {
-                return res.status(400).json({message : "Missing parameter"});
-            }   
-            
-            // user exist ?
-            const ifUser = await User.findOne({email : email, username : username});
-            if(ifUser) {
-                return res.status(409).json({message : "This email is already used"});
-               
-            } 
-        
-            // generate salt
-            const token = uid2(64);
-            const salt = uid2(16);
-            const hash = SHA256(salt + password).toString(encBase64);
-
-
-            // // convert picture
-            // const pictureConverted = convertToBase64(req.files.picture);
-
-            // // send file
-            // const result = await cloudinary.uploader.upload(pictureConverted, {
-            //     folder: "/vinted/avatar",
-            // });
-
-            const newUser = new User({
-                email,
-                account: {
-                    username,
-                    // avatar: result.secure_url, 
-                },
-                newsletter,
-                token,
-                hash,
-                salt,
-            });
-
-            await newUser.save();
-            res.json({
-                _id: newUser.id,
-                token: newUser.token,
-                account: newUser.account
-            });
-        } catch (error) {
-            res.status(400).json({message : error.message});
-        }
+    const newUser = new User({
+      email: req.body.email,
+      account: {
+        username: req.body.username,
+        //avataer : (img)
+      },
+      newsletter: req.body.newsletter,
+      token: token,
+      hash: hash,
+      salt: salt,
+    });
+    //console.log(newUser);
+    const pictureToAvatar = convertToBase64(req.files.picture);
+    //console.log(pictureToAvatar);
+    const resultAvatar = await cloudinary.uploader.upload(pictureToAvatar, {
+      folder: `/users${newUser._id}`,
     });
 
-// Read
-    // login
-    router.post('/user/login', async (req,res) => {
-        try {
-            const {email, password} = req.body;
-            const user = await User.findOne({email : email});
-            // user exist ?
-            if(!user) {
-                return res.status(401).json({message : "Unauthorized"});
-            } 
-        
-            // reconstitute password-hash
-            const newHash = SHA256(user.salt + password).toString(encBase64);
+    //console.log(resultAvatar);
+    newUser.account.avatar = resultAvatar;
 
-            if(newHash !== user.hash) {
-                return res.status(401).json({message : "Le mot de passe n'est pas correct ❌"}); 
-            } 
-            
-            res.json({
-                _id: user._id,
-                token: user.token,
-                account: user.account,
-              });
-        } catch (error) {
-            res.status(400).json({message : error.message});
-        }
-    })
+    await newUser.save();
+
+    console.log(newUser);
+    const response = {
+      id: newUser._id,
+      token: token,
+      account: newUser.account,
+    };
+    res.json(response);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.post("/user/login", async (req, res) => {
+  //console.log(req.body);
+  try {
+    if (!req.body.email || !req.body.password) {
+      return res.status(400).json({ message: "The element missing" });
+    }
+    const user = await User.findOne({ email: req.body.email });
+    //console.log(user);
+    if (user) {
+      const userPassword = req.body.password;
+      //console.log(userPassword);
+      const userSalt = user.salt;
+      //console.log(userSalt);
+      userHash = SHA256(userSalt + userPassword).toString(encBase64);
+      //console.log(userHash);
+      //console.log(userHash === user.hash);
+      if (userHash !== user.hash) {
+        return res
+          .status(400)
+          .json({ message: "The email or password isn't correct" });
+      }
+
+      const responseUser = {
+        id: user._id,
+        token: user.token,
+        account: user.account,
+      };
+      res.json(responseUser);
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 
 module.exports = router;
